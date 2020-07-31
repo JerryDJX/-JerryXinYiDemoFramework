e79c9fb8d49176ae064197a766751b5ab698ddce
豆佳兴 <doujiaxing@people.cn>
2020-07-31 16:09:17 +0800

    Initial Commit

---
+++ JerryXinYiDemoFramework/JerryXinYiDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  JerryXinYiDemoFramework.h
+//  JerryXinYiDemoFramework
+//
+//  Created by DJX on 2020/7/31.
+//  Copyright © 2020 豆佳兴. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JerryXinYiDemoFramework.
+FOUNDATION_EXPORT double JerryXinYiDemoFrameworkVersionNumber;
+
+//! Project version string for JerryXinYiDemoFramework.
+FOUNDATION_EXPORT const unsigned char JerryXinYiDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JerryXinYiDemoFramework/PublicHeader.h>
+
+
